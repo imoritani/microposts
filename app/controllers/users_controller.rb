d9a55afa9773ad22167a148eb2630c2159b7ad73
Ikuo <i.moritani@gmail.com>
2016-10-23 21:52:52 +0000

    add edit function.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show, :edit, :update]
+
   def new
     @user = User.new
-  end
-  
-  def show
-    @user = User.find(params[:id])
   end
   
   def create
@@ -17,9 +15,30 @@ class UsersController < ApplicationController
     end
   end
   
+  
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:info] = "プロフィールを編集しました."
+      redirect_to @user
+    else
+      
+    end
+  end
+  
+  
 private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
